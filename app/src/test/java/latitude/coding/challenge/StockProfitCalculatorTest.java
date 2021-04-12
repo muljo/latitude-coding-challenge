65882c224f364adbf2e73d50882ed303d7d397f7
Josh Mullin <jmull0@protonmail.com>
2021-04-12 15:52:25 +1200

    remove displayname and before each imports from test file

--- app/src/test/java/latitude/coding/challenge/StockProfitCalculatorTest.java
+++ app/src/test/java/latitude/coding/challenge/StockProfitCalculatorTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.BeforeEach;
 
 import java.util.stream.IntStream;
 
